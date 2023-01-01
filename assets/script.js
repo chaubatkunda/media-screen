447d37e2df4aa8ae5e39427a81fe83e1748b133b
chaubatkunda <maniklusi31@gmail.com>
2023-01-01 20:04:24 +0700

    update screen

--- assets/script.js
+++ assets/script.js
@@ -187,5 +187,6 @@ var alertModal = document.querySelector("#modal-alert");
 var canvas = document.querySelector("#unity-canvas");
 btnWake.addEventListener("click", function () {
   canvas.style.display = "block";
+  canvas.style.overflow = "hidden";
   alertModal.classList.remove("active");
 });
